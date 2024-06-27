c72c6e466709eb062e97d3d3a19dc10be12c88eb
sachinsingh4421 <158204030+sachinsingh4421@users.noreply.github.com>
2024-06-27 22:05:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+# Function to add two numbers
+def add(x, y):
+    return x + y
+
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero"
+    else:
+        return x / y
+
+# Function to power of two numbers
